0bf7ffccb44bcfe156cdd74c704c5962f3e562bd
ayaszmr <s.aya@live.com>
2020-04-25 18:37:45 +0900

    マイグレーションファイル編集

--- db/migrate/20200425091843_create_caddies.rb
+++ db/migrate/20200425091843_create_caddies.rb
@@ -1,7 +1,9 @@
 class CreateCaddies < ActiveRecord::Migration[5.0]
   def change
     create_table :caddies do |t|
-
+      t.string :name
+      t.integer :caddy_id
+      t.integer :bags
       t.timestamps
     end
   end
