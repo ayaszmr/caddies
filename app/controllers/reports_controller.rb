cb162fbad36341ced1879c2a53ae77beb06b2c12
ayaszmr <s.aya@live.com>
2020-05-22 15:31:46 +0900

    user_idカラム追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,8 @@
 class ReportsController < ApplicationController
   def index
     @reports = Report.all
+    # @reports = Report.group_by{|report| report.created_at.to_date}.values
+    # @reports = Order.select("date(created_at) as ordered_date").group("date(created_at)")
   end
   
   def new
