b7860c3b233f4e780aa7d4c03924fb20c9c10ea2
ayaszmr <s.aya@live.com>
2020-05-22 15:50:27 +0900

    コントローラー編集

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,8 @@ class ReportsController < ApplicationController
 
   def create
     @report = Report.new(report_params)
-    if @report.save
+    # @report = Report.new(caddy_id: report_params[:caddy_id], bags: report_params[:bags], user_id: current_user.id)
+    if @report.save(user_id: current_user.id)
       redirect_to reports_path
     else
       render 'new'
