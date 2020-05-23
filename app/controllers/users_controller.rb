08a549aa94506dfec17f8ddc5fa3823c4a3e4032
ayaszmr <s.aya@live.com>
2020-05-23 14:23:03 +0900

    マイページbag数の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    
+    # @user = current_user.name
+    @bags = Report.where(user_id: current_user.id).sum(:bags)
   end
 end
