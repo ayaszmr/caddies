39c423c1aa0e2035555795c768bc36fc8c360c94
ayaszmr <s.aya@live.com>
2020-06-28 13:41:06 +0900

    searchのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :reports, only: [:new, :create, :index]
+  resources :reports, only: [:new, :create, :index] do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: [:index, :show]
 end
