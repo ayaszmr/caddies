ab5c38f5d04fe0af58bec8140b88acfcb6ccf4b8
ayaszmr <s.aya@live.com>
2020-07-06 20:53:12 +0900

    form_forにurl追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,5 +5,16 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
