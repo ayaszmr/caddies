9a545ca3c0949b75e698ad7fba53a4e92399ef23
ayaszmr <s.aya@live.com>
2020-07-06 11:27:37 +0900

    edit update追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @bags = Report.where(user_id: current_user.id).sum(:bags)
     @days = Report.where(user_id: current_user.id).count
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
