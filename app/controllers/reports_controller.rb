b414c737b32da6afc6a0a5d0c41d9271e4a8dd63
ayaszmr <s.aya@live.com>
2020-07-13 13:33:26 +0900

    出番表データ挿入

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,13 @@ require 'date'
 class ReportsController < ApplicationController
   before_action :set_ransack
   def index
-    @bags = Report.order(bags: "ASC").where(round_at: Date.today)
+    @bags = Report.order(bags: "ASC").where(round_at: Date.today).first(5)
+    @bagss = Report.order(bags: "ASC").where(round_at: Date.today).limit(5).offset(5)
+    @bagsss = Report.order(bags: "ASC").where(round_at: Date.today).limit(5).offset(10)
+    @bagssss = Report.order(bags: "ASC").where(round_at: Date.today).limit(5).offset(15)
+    @bagsssss = Report.order(bags: "ASC").where(round_at: Date.today).limit(5).offset(20)
+    @bagssssss = Report.order(bags: "ASC").where(round_at: Date.today).limit(5).offset(25)
+
 
     @q=Report.ransack(params[:q])
     @reports=@q.result(distinct: true)
